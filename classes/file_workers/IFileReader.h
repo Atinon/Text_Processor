58da85e8665dd120dab423c91c3e8641612b2237
Nikola <nkl_andonov@yahoo.com>
2023-06-20 21:35:23 +0300

    Implemented FileReader Interface

--- classes/file_workers/IFileReader.h
+++ classes/file_workers/IFileReader.h
@@ -1,9 +1,18 @@
 #ifndef TEXT_PROCESSOR_IFILEREADER_H
 #define TEXT_PROCESSOR_IFILEREADER_H
 
+#include <vector>
+#include <string>
 
 class IFileReader {
+public:
+    virtual std::vector<std::string> readFileContent() = 0;
 
+    virtual const std::string &getFileName() = 0;
+
+    virtual void setFileName(const std::string &fileName) = 0;
+
+    virtual ~IFileReader() = default;
 };
 
 
