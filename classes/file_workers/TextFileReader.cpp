b96c9c1554c286e7c0f2a9ba3dc9583e60dcfc08
Nikola <nkl_andonov@yahoo.com>
2023-06-20 20:51:54 +0300

    Initial TextFileReader implementation

---
+++ classes/file_workers/TextFileReader.cpp
@@ -0,0 +1,33 @@
+#include "TextFileReader.h"
+
+const std::string &TextFileReader::getFileName() {
+    return fileName_;
+}
+
+void TextFileReader::setFileName(const std::string &fileName) {
+    fileName_ = fileName;
+}
+
+std::vector<std::string> TextFileReader::readFileContent() {
+    std::vector<std::string> result;
+    std::string currentLine;
+    std::fstream in(fileName_);
+
+    if(!in.is_open()){
+        throw std::runtime_error("Failed to open the file: " + fileName_);
+    }
+
+    while(getline(in, currentLine, '\n')){
+        try{
+            result.push_back(currentLine);
+        }
+        catch(const std::bad_alloc&){
+            result.clear();
+            throw std::runtime_error("File too big to load. File name: " + fileName_);
+        }
+    }
+
+    in.close();
+    return result;
+}
+
---
+++ classes/file_workers/TextFileReader.h
@@ -0,0 +1,25 @@
+#ifndef TEXT_PROCESSOR_TEXTFILEREADER_H
+#define TEXT_PROCESSOR_TEXTFILEREADER_H
+
+
+#include "IFileReader.h"
+#include <fstream>
+
+class TextFileReader : public IFileReader {
+private:
+    std::string fileName_;
+
+public:
+    explicit TextFileReader(const std::string &fileName) : fileName_(fileName) {};
+
+    const std::string &getFileName() override;
+
+    void setFileName(const std::string &fileName) override;
+
+    std::vector<std::string> readFileContent() override;
+
+    ~TextFileReader() override = default;
+};
+
+
+#endif //TEXT_PROCESSOR_TEXTFILEREADER_H
