e365d24abc0d2db21cd347977c0fe5c8fbeb793a
Nikola <nkl_andonov@yahoo.com>
2023-06-20 21:34:43 +0300

    Created Config class

--- classes/Config.cpp
+++ classes/Config.cpp
@@ -1,1 +1,19 @@
 #include "Config.h"
+#include "file_workers/TextFileReader.h"
+
+DefaultConfig::DefaultConfig() {
+    try {
+        fileReader_ = new TextFileReader();
+    }
+    catch (const std::bad_alloc &) {
+        fileReader_ = nullptr;
+    }
+}
+
+IFileReader *DefaultConfig::getFileReader() {
+    return fileReader_;
+}
+
+DefaultConfig::~DefaultConfig() {
+    delete fileReader_;
+}
--- classes/Config.h
+++ classes/Config.h
@@ -2,11 +2,27 @@
 #define TEXT_PROCESSOR_CONFIG_H
 
 
+#include "file_workers/IFileReader.h"
 
-class Config {
+class IConfig {
+public:
+    virtual IFileReader* getFileReader() = 0;
 
+    virtual ~IConfig() = default;
 };
 
+
+class DefaultConfig : public IConfig {
+private:
+    IFileReader *fileReader_;
+
+public:
+    DefaultConfig();
+
+    IFileReader *getFileReader() override;
+
+    ~DefaultConfig() override;
+};
 
 
 #endif //TEXT_PROCESSOR_CONFIG_H
